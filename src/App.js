3ba8df4caa9a333237aacd8e83fc27b4c03fe4e5
Sandeepan Bhattacharyya <bsandeepan95.work@gmail.com>
2020-08-14 21:11:43 +0530

    basic components and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import {Route, Switch} from 'react-router-dom';
+
+import Header from './components/header/header.component';
+import HomePage from './pages/home/home.component';
+import ShopPage from './pages/shop/shop.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={ShopPage}/>
+        <Route path='/shop/:name' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
-import './homepage.styles.scss';
+import './home.styles.scss';
 
 const HomePage = () => (
     <div className='homepage'>
